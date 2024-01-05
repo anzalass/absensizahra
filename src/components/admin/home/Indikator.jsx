a3f5e2901f255e5f448a497cb5faa16f38a4acdf
anzalass <109114576+anzalass@users.noreply.github.com>
2024-01-05 13:28:51 +0700

    Update Indikator.jsx

--- src/components/admin/home/Indikator.jsx
+++ src/components/admin/home/Indikator.jsx
@@ -256,7 +256,9 @@ export default function Indikator() {
       ) : user?.role === 5 ? (
         <div className="mt-3 p-3 w-full mx-auto bg-white rounded-lg h-[40vh]  block lg:flex xl:flex md:flex ">
           <div className=" grid grid-cols-2 gap-4  mx-auto lg:w-[50%] xl:w-[50%] md:w-[50%] w-full ">
-            <div className="h-[100px] pl-3 pt-6 relative z-30 rounded-md w-[100%] bg-[#155f95]">
+
+            <Link to={"/AllUsers"}>
+              <div className="h-[100px] pl-3 pt-6 relative z-30 rounded-md w-[100%] bg-[#155f95]">
               <h1 className=" text-white font-[500] text-[20px]">
                 {kurikulum?.siswa}
               </h1>
@@ -264,6 +266,9 @@ export default function Indikator() {
                 Daftar Siswa
               </h1>
             </div>
+            </Link>
+            
+            <Link to={"/AllUsers"}>
             <div className="h-[100px] pl-3 pt-6 relative rounded-md w-[100%] bg-[#FDB022]">
               <h1 className=" text-white font-[500] text-[20px]">
                 {kurikulum?.guru}
@@ -272,8 +277,10 @@ export default function Indikator() {
                 Daftar Guru
               </h1>
             </div>
-
-            <div className="h-[100px] pl-3 pt-6 relative rounded-md w-[100%] bg-[#32D583]">
+            </Link>
+            
+            <Link to={"/AllUsers"}>
+           <div className="h-[100px] pl-3 pt-6 relative rounded-md w-[100%] bg-[#32D583]">
               <h1 className=" text-white font-[500] text-[20px]">
                 {kurikulum?.admin}
               </h1>
@@ -281,6 +288,9 @@ export default function Indikator() {
                 Daftar Admin
               </h1>
             </div>
+            </Link>
+            
+            <Link to={"/AllUsers"}>
             <div className=" pl-3 pt-6 h-[100px] relative rounded-md w-[100%] bg-[#F04438]">
               <h1 className=" text-white font-[500] text-[20px]">
                 {kurikulum?.kurikulum}
@@ -289,6 +299,11 @@ export default function Indikator() {
                 Daftar Kurikulum
               </h1>
             </div>
+            </Link>
+
+
+ 
+
             <Link to={"/PermintaanIzin"}>
               <div className="h-[100px] pl-3 pt-6 relative rounded-md w-[100%] bg-purple-500">
                 <h1 className=" text-white font-[500] text-[20px]">
